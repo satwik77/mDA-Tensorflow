1057ee687c3a101bd34daa4fd6f05dd750bcf47e
satwik77 <satwik55@gmail.com>
2018-03-22 00:20:15 +0530

    marginalized DA added

--- mDA.py
+++ mDA.py
@@ -4,15 +4,13 @@ import tensorflow as tf
 import numpy as np
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 
-def denoising_autoencoder(hidden_units= 1000, prob=0.4):
+def mDA(hidden_units= 1000, noiserate=0.4):
 
 	learning_rate = 0.005
 	batch_size = 50
 	n_epochs = 300
 
 	x = tf.placeholder(tf.float32, [None, 784], name='x')
-
-	x_ = tf.add(x ,np.random.normal(loc= 0.0, scale=prob , size= (batch_size, 784) ))
 
 	n_inp = 784
 	n_out = hidden_units
@@ -22,12 +20,21 @@ def denoising_autoencoder(hidden_units= 1000, prob=0.4):
 	W_ = tf.Variable(tf.random_uniform([n_out, n_inp], -1.0 / np.sqrt(n_inp), 1.0 / np.sqrt(n_inp)) ,dtype=tf.float32 )
 	b_ = tf.Variable(tf.truncated_normal([n_inp], dtype=tf.float32))
 
-	z = tf.nn.sigmoid(tf.matmul(x_ , W) + b)
+	z = tf.nn.sigmoid(tf.matmul(x , W) + b)
 
 	y = tf.nn.sigmoid(tf.matmul(z , W_) + b_)
 
 
-	cost = tf.reduce_mean(-tf.reduce_sum(x * tf.log(tf.clip_by_value(y ,1e-10,1.0))) )
+	L = tf.reduce_mean(-tf.reduce_sum(x * tf.log(tf.clip_by_value(y ,1e-10,1.0))) )
+
+	dz = z * (1-z)
+	dy = y * (1-y)
+
+	dfx_2 = tf.matmul(tf.matmul(dy, W * W) * dz * dz, W_ * W_)
+
+	L2 = noiserate * noiserate * tf.reduce_mean(tf.reduce_sum(dfx_2, 1))
+
+	cost = L + 0.5 * L2
 
 	print("Fetching Data...")
 	mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
@@ -35,14 +42,12 @@ def denoising_autoencoder(hidden_units= 1000, prob=0.4):
 
 	print("Starting Session...")
 
-	saver = tf.train.Saver()
-
 	optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
 	sess = tf.Session()
 	sess.run(tf.global_variables_initializer())
 
 	# saver.restore(sess, "./weights/da.cpkt")
-
 
 	for i in range(n_epochs):
 		avg_cost = 0
@@ -51,7 +56,7 @@ def denoising_autoencoder(hidden_units= 1000, prob=0.4):
 		for batch_i in range(batches):
 			batch_xs, _ = mnist.train.next_batch(batch_size)
 			# train = np.array([img - mean_img for img in batch_xs])
-			_, ce = sess.run([optimizer, cost], feed_dict={x : batch_xs})
+			_,ce = sess.run([optimizer, cost], feed_dict={x: batch_xs})
 
 			avg_cost += ce / batches
 
@@ -59,6 +64,8 @@ def denoising_autoencoder(hidden_units= 1000, prob=0.4):
 
 
 
+	save = saver.save(sess, "./weights/mda.ckpt")
+
 
 if __name__ == '__main__':
-	denoising_autoencoder()
+	mDA()
